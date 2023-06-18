822043db8efce8db667962c7413e409d48e32308
JotaPrimo <gestald118@gmail.com>
2023-06-17 21:16:20 -0400

    14. Creating our first API Endpoint

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -1,56 +1,43 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Controller;
 use App\Models\Task;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 
 class TaskController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     */
+{    
     public function index()
     {
-        //
+        return Task::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+   
     public function create()
     {
         //
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
+   
     public function store(StoreTaskRequest $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
+   
     public function show(Task $task)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+   
     public function edit(Task $task)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+   
     public function update(UpdateTaskRequest $request, Task $task)
     {
         //
