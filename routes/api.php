aeab8a83f1a68715ea179e3770ed18df22594278
JotaPrimo <gestald118@gmail.com>
2023-06-18 11:45:41 -0400

    24. Using Sanctum for API Token Authentication

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
+
 use App\Http\Controllers\Api\V1\CompleteTaskContrller;
 use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +24,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::apiResource('/tasks', TaskController::class);
     Route::patch('/tasks/{task}/complete', CompleteTaskContrller::class);
+});
+
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
+    Route::post('/register', RegisterController::class);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
