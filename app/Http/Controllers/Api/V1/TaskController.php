3b4e7ed905b869d8f0dd7d7e9f5fea7a3f047a89
JotaPrimo <gestald118@gmail.com>
2023-06-17 21:41:37 -0400

    18. Deleting Data

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -44,6 +44,8 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->noContent();
     }
 }
